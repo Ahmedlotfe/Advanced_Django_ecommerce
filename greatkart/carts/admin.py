b6a10d8977a80cb09bbff9f21020a2ebe0289d35
Ahmedlotfe <ahmedlotfe132@gmail.com>
2021-11-26 12:19:06 +0200

    Updating

---
+++ greatkart/carts/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['cart_id', 'date_added']
+
+
+@admin.register(models.CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['product', 'cart', 'quantity', 'is_active']
